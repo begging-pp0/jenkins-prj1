83b0f9282f2a2a339dce1f8124cb5e3381c49593
begging-pp0 <pp0begging@gmail.com>
2024-08-17 19:05:39 +0000

    For Java Application and its test code

---
+++ src/test/java/com/vcjain/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kk.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
